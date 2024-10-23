b139240ed5e527b13ee7a8ef1bcb74a7991103b1
dddaaazzz <darya2kolesnikova@gmail.com>
2024-10-23 16:32:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ document.addEventListener("DOMContentLoaded", () => {
         messageDisplay.innerHTML = ""; // Очистка сообщения
 
         // Проверка на пустой ввод или длину
-        if (letterInputValue === "" || letterInputValue.length !== 1) {
-            messageDisplay.innerHTML = "Введите одну букву!";
+        if (letterInputValue === "" || letterInputValue.length !== 1 || !/^[а-яА-ЯёЁ]$/.test(letterInputValue)) {
+            messageDisplay.innerHTML = "Введите одну букву на кириллице!";
             return;
         }
 
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
             hangmanParts[6 - lives].style.display = "block";
 
             if (lives === 0) {
-                messageDisplay.innerHTML = `Вы проиграли! Загаданное слово было: ${selectedWord}`;
+                messageDisplay.innerHTML =const letterInputValue = letterInput.value.toLowerCase()
                 letterInput.disabled = true;
                 guessBtn.disabled = true;
             }
